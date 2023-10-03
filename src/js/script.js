867de36814a2ec322086bd6150a556c4986e011d
gomesvt <142927205+gomesvt@users.noreply.github.com>
2023-10-03 12:27:10 -0300

    Add files via upload

---
+++ src/js/script.js
@@ -0,0 +1,25 @@
+const botoes = document.querySelectorAll('.botao');
+const personagens = document.querySelectorAll('.personagem');
+
+botoes.forEach((botao, indice) => {
+    botao.addEventListener("click", () => {
+        desSelecionarBotao();
+        desSelecionarPersonagem();
+
+        botao.classList.add("selecionado");
+        personagens[indice].classList.add("selecionado");
+    });
+});
+
+function desSelecionarPersonagem() {
+    const personagemSelecionado = document.querySelector(".personagem.selecionado");
+    personagemSelecionado.classList.remove("selecionado");
+}
+
+function desSelecionarBotao() {
+    const botaoSelecionado = document.querySelector(".botao.selecionado");
+    botaoSelecionado.classList.remove("selecionado");
+}
+
+
+
